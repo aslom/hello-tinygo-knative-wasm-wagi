6c2504b95a3fab34139265fb4444d9938c82ed53
Aleksander Slominski <aslom@us.ibm.com>
2023-02-28 09:52:54 -0500

    sample code

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("Content-Type: text/plain")
+	fmt.Println("")
+	fmt.Println("Hello Code Engine in Wasm World!")
+
+	printenv()
+}
+
+func printenv() {
+	//fmt.Printf("HOME=%s\n", os.Getenv("HOME"))
+	//fmt.Println(os.Environ())
+	for _, e := range os.Environ() {
+		fmt.Printf("%s\n", e)
+	}
+}
